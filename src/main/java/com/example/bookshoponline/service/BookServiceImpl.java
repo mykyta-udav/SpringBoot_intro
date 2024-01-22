471e8c0e8034fff9c0600c774898b6f67bb12e6f
mykyta-udav <nikitaudav123@gmail.com>
2024-01-22 15:21:09 +0200

    Implemented Book entity, service and repository

---
+++ src/main/java/com/example/bookshoponline/service/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.bookshoponline.service;
+
+import com.example.bookshoponline.model.Book;
+import com.example.bookshoponline.repository.BookRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
